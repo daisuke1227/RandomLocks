40fc6d71f4857ed91e917f982e78fbd5415318ce
daisuke1227 <144384874+daisuke1227@users.noreply.github.com>
2024-04-10 02:32:33 -0400

    Create Universal.lua

---
+++ Universal.lua
@@ -0,0 +1,18 @@
+--[[
+ ░█████╗░░█████╗░███╗░░░███╗    ██╗░░░░░░█████╗░░█████╗░██╗░░██╗
+██╔══██╗██╔══██╗████╗░████║    ██║░░░░░██╔══██╗██╔══██╗██║░██╔╝
+██║░░╚═╝███████║██╔████╔██║    ██║░░░░░██║░░██║██║░░╚═╝█████═╝░
+██║░░██╗██╔══██║██║╚██╔╝██║    ██║░░░░░██║░░██║██║░░██╗██╔═██╗░
+╚█████╔╝██║░░██║██║░╚═╝░██║    ███████╗╚█████╔╝╚█████╔╝██║░╚██╗
+░╚════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝    ╚══════╝░╚════╝░░╚════╝░╚═╝░░╚═╝
+  Made by: Space#0234
+Discord Server: .gg/966295RjzW
+Website: https://triphub.ga/
+Support: Mobile/Pc
+]]--
+  getgenv().Prediction = 1
+getgenv().AimPart = "LowerTorso"
+getgenv().Key = "q"
+getgenv().AutoPrediction = true
+loadstring(game:HttpGet('https://raw.githubusercontent.com/elxocasXD/Trip-Hub/main/Scripts/Cam%20Lock.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
